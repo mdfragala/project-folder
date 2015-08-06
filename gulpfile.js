9e7e75ca59c1eb5d9042ea047fce0fff3333ce33
Mitchell Gates <md.fragala@gmail.com>
2015-08-06 14:37:01 -0400

    S

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,25 @@
-var gulp = require('gulp'), $ = require('gulp-load-plugins')({patterns:['*']});
+var gulp = require('gulp'), $ = require('gulp-load-plugins')({pattern:['*']});
 
 var templates = {
    src: ['./src/**/*.html', '!./src/assets/_layouts/**/*.html', '!./src/assets/_includes/**/*.html']
 }
 
 gulp.task('template', function(){
-      return gulp.src(templates.src) {
-      .pipe(wrap())
-   }
+   $.nunjucksRender.nunjucks.configure(['./_src/assets/_layouts'],{watch:false});
+   return gulp.src('./_src/assets/_layouts/default.html')
+      .pipe($.data(function(file){
+         var content = $.frontMatter(String(file.contents));
+         file.contents = new Buffer(content.body);
+         return {'meta': content.attributes};
+      }))
+      .pipe($.insert.transform(function(contents, file){
+         var extend, block, end, layout;
+         layout = file.data.meta.layout || 'default';
+         extend = '{% extends "default.html" %}';
+         block = '{% block content %}\n';
+         end = '{% endblock %}';
+         return extend + block + contents + end;
+      }))
+      .pipe($.nunjucksRender())
+      .pipe(gulp.dest('./_dist/'));
 });
